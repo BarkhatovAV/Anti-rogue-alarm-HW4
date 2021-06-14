b0edc960fb109532327002e7ece3e744a7c81a1e
BarkhatovAV <barkhatov.a.v@mail.ru>
2021-06-14 14:28:18 +0300

    Update Entrance

--- Assets/Scripts/Entrance.cs
+++ Assets/Scripts/Entrance.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 
 public class Entrance : MonoBehaviour
@@ -10,7 +8,6 @@ public class Entrance : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        int durationOfVolumeDown = 4;
         if (collision.TryGetComponent(out PlayerMover player))
         {
             _entered?.Invoke();
